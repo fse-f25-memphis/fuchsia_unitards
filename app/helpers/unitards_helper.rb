05fc90ed158590f7076d4b46a547e6ccc281b78e
shashwatdadhich <dadhichshashwat1808@gmail.com>
2025-11-23 21:45:10 -0600

    Images added

--- app/helpers/unitards_helper.rb
+++ app/helpers/unitards_helper.rb
@@ -2,15 +2,29 @@ module UnitardsHelper
     def unitard_image(unitard)
       case unitard.name
       when "He-Man Power Suit 1"
-        "unitards/suit1.jpg"        # or .jpg if that's what you have
+        "unitards/suit1.jpg"        
       when "He-Man Power Suit 2"
         "unitards/suit2.jpg"
-      when "She-Ra"
-        "unitards/unitard_battlecat_green.jpg"
-      when "Battle Cat"
-        "unitards/unitard_battlecat_green.jpg"  # 🔴 changed from .svg → .png
-      when "Castle Grayskull"
-        "unitards/unitard_battlecat_green.jpg"
+      when "She-Ra Power Suit 3"
+        "unitards/suit3.jpg"
+      when "She-Ra Power Suit 4"
+        "unitards/suit4.jpg"  
+      when "Castle Grayskull Power Suit 5"
+        "unitards/suit5.jpg"
+      when "Battle Cat Power Suit 6"
+        "unitards/suit6.jpg"
+      when "Skeletor Power Suit 7"
+        "unitards/suit7.jpg"
+      when "Castle Grayskull Power Suit 8"
+        "unitards/suit8.jpg"
+      when "Man-At-Arms Power Suit 9"
+        "unitards/suit9.jpg"
+      when "Man-At-Arms Power Suit 10"
+        "unitards/suit10.jpg"
+      when "He-Man Power Suit 11"
+        "unitards/suit11.jpg"
+      when "Man-At-Arms Power Suit 12"
+        "unitards/suit12.jpg"
       else
         "unitards/unitard_battlecat_green.jpg"
       end
