4c13c114695881347703750020ca7851bce0937a
shashwatdadhich <dadhichshashwat1808@gmail.com>
2025-11-23 20:31:45 -0600

    images added

--- app/helpers/unitards_helper.rb
+++ app/helpers/unitards_helper.rb
@@ -1,10 +1,10 @@
 module UnitardsHelper
     def unitard_image(unitard)
-      case unitard.graphic
-      when "He-Man"
-        "unitards/unitard_battlecat_green.jpg"        # or .jpg if that's what you have
-      when "Skeletor"
-        "unitards/unitard_battlecat_green.jpg"
+      case unitard.name
+      when "He-Man Power Suit 1"
+        "unitards/suit1.jpg"        # or .jpg if that's what you have
+      when "He-Man Power Suit 2"
+        "unitards/suit2.jpg"
       when "She-Ra"
         "unitards/unitard_battlecat_green.jpg"
       when "Battle Cat"
